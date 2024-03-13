3eae6ffe5103fe958aa362a05289c72f732013d3
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-03-13 13:09:59 +0530

    fixed minute error

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -3,7 +3,7 @@ import { Balance } from "../css_components/Balance"
 import Appbar from '../css_components/Appbar'
 import {User} from '../components/user'
 export default function Dashboard(){
-    const balance=useRecoilValue(getB)
+    // const balance=useRecoilValue(getB)
     return (
         <div>
             <Appbar/>
