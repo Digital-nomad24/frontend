5e26e35d6be2b887ee39148cb7cd98c4ee4ff71a
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-03-14 09:21:01 +0530

    modified navigate

--- src/components/send.jsx
+++ src/components/send.jsx
@@ -53,7 +53,7 @@ export default function Send()  {
                         })
                         .then(res=>{
                             console.log(res.data)
-                            navigate('/dashboard')
+                            window.location.href='/dashboard'
                         })
                     }} className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Initiate Transfer
