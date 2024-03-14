8e9572211778cc15391cf3a5d6cd18fe67e19fb1
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-03-15 01:12:12 +0530

    dashboard modified

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -10,6 +10,7 @@ export default function Dashboard(){
             <Appbar/>
             <br />
             <h3><Balance value={balance}></Balance></h3>
+            {console.log(balance)}
             {console.log("SEPERATION BTW")}
             <User></User>
         </div>
