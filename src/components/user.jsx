e623227fbad8ad0af926165c8583a4a8e4053e7a
Nakshatra Vashisth <nakshvashisth@gmail.com>
2024-04-09 15:56:59 +0530

    deleted useless file

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -6,24 +6,31 @@ import { filteratom } from "./atoms";
 import { useRecoilState } from "recoil";
 import { userfilter } from "./atoms";
 import { useNavigate } from "react-router-dom";
+import { usernameatom } from "./atoms";
 export function User()
 {
+    const [username,setusername]=useRecoilState(usernameatom)
     const [users,setUser]=useRecoilState(userfilter)
     const[filter,setfiltervalue]=useRecoilState(filteratom)
     useEffect(() => {
-        axios.get(`https://backend-1-79iw.onrender.com/api/v1/user/filter?filter=${filter}`)
+        axios.get(`http://localhost:3000/api/v1/user/filter?filter=${filter}`)
           .then(res => {
             setUser(res.data.user)
           })
           .catch(error => {
             console.error('Error fetching data:', error);
           });
-      }, [filter]); // Run effect whenever filter changes
+      }, [filter]);
     return <div>
         <InputBox label={"enter a name to search your friend"} placeholder={'john'} onChange={(e)=>{
             setfiltervalue(e.target.value)
         }} type="text" />
-            {users.map(e=><DisplayUser key={Math.random()} user={e}/>)}
+        {users.filter((user) => {
+        return user.username!=username
+       }).map((filteredUser) => (
+       <DisplayUser key={Math.random()} user={filteredUser} />
+       ))}
+
             </div>
         }
 function DisplayUser({user}){
@@ -32,7 +39,7 @@ function DisplayUser({user}){
     <div className="flex">
     <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
         <div className="flex flex-col justify-center h-full text-xl">
-            {user.firstName}
+            {String(user.firstName).charAt(0)}
         </div>
     </div>
     <div className="flex flex-col justify-center h-ful">
